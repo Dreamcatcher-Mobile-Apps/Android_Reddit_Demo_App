9d35e61e72557b5efb286d2a28b6f4035feea0a3
Rafal Ozog <rafalozog@gmail.com>
2022-12-07 23:49:43 +0000

    Uncommited changes - commited

--- SourceCode/app/src/main/java/redditandroidapp/features/feed/PostsListAdapter.kt
+++ SourceCode/app/src/main/java/redditandroidapp/features/feed/PostsListAdapter.kt
@@ -1,17 +1,24 @@
 package redditandroidapp.features.feed
 
 import android.content.Context
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.unit.dp
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import kotlinx.android.synthetic.main.main_feed_list_item.view.*
-import redditandroidapp.R
+import coil.compose.rememberAsyncImagePainter
+import com.google.android.material.composethemeadapter.MdcTheme
 import redditandroidapp.data.database.PostDatabaseEntity
 
 // Main adapter used for managing items list within the main RecyclerView (main feed listed)
-class PostsListAdapter (private val context: Context, val clickListener: (Int) -> Unit) : RecyclerView.Adapter<ViewHolder>() {
+class PostsListAdapter (private val context: Context, val clickListener: (Int) -> Unit) : RecyclerView.Adapter<ComposeViewHolder>() {
 
     private var postsList: List<PostDatabaseEntity> = ArrayList()
 
@@ -39,33 +46,31 @@ class PostsListAdapter (private val context: Context, val clickListener: (Int) -
         return postsList.size
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context)
-            .inflate(R.layout.main_feed_list_item, parent, false)
-        )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ComposeViewHolder {
+        return ComposeViewHolder(ComposeView(parent.context))
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ComposeViewHolder, position: Int) {
 
-        // Prepare fetched data
+        // Prepare data to display
         val title = postsList[position].title
         val author = postsList[position].author
         val urlToImage = postsList[position].thumbnail
 
-        // Set the picture
-        Glide.with(context)
-            .load(urlToImage)
-            .into(holder.picture)
+        holder.bind(title, author, urlToImage)
 
-        // Set data within the holder
-        holder.title.text = title
-        holder.author.text = author
+//        // Set onClickListener
+//        holder.rowContainer.setOnClickListener{
+//            val itemId = postsList[position].id
+//            clickListener(itemId)
+//        }
+    }
 
-        // Set onClickListener
-        holder.rowContainer.setOnClickListener{
-            val itemId = postsList[position].id
-            clickListener(itemId)
-        }
+    // Jetpack Compose requirement
+    override fun onViewRecycled(holder: ComposeViewHolder) {
+        // Dispose the underlying Composition of the ComposeView
+        // when RecyclerView has recycled this ViewHolder
+        holder.view.disposeComposition()
     }
 
     fun getLastPostId(): String? {
@@ -73,9 +78,31 @@ class PostsListAdapter (private val context: Context, val clickListener: (Int) -
     }
 }
 
-class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
-    val picture = view.imageView_picture
-    val title = view.textView_title
-    val author = view.textView_author
-    val rowContainer = view.row_container
+class ComposeViewHolder (val view: ComposeView) : RecyclerView.ViewHolder(view) {
+
+    // Jetpack Compose requirement
+    init {
+        view.setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+    }
+
+    fun bind(title: String?, author: String?, picture: String?) {
+        view.setContent {
+            Row {
+                Column {
+                    picture?.let { Image(
+                        painter = rememberAsyncImagePainter(it),
+                        contentDescription = null,
+                        modifier = Modifier.size(128.dp)
+                    ) }
+                }
+                Column {
+                    MdcTheme {
+                        // Todo: Unify Mdc and MaterialTheme?
+                        title?.let { Text(text = it, style = MaterialTheme.typography.subtitle1) }
+                        author?.let { Text(text = it, style = MaterialTheme.typography.subtitle2) }
+                    }
+                }
+            }
+        }
+    }
 }
