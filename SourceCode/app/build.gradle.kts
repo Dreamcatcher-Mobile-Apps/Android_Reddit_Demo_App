c037744c743469c1d3bcf397f16475706528c2b1
Rafal Ozog <rafalozog@gmail.com>
2022-12-31 17:11:17 +0000

    Code cleaning - unused dependencies removed.

--- SourceCode/app/build.gradle.kts
+++ SourceCode/app/build.gradle.kts
@@ -2,8 +2,8 @@ android {
     compileSdk = libs.versions.compileSdk.get().toInt()
     defaultConfig {
         applicationId = "development.dreamcatcher.redditandroidapp"
-        minSdk = 21
-        targetSdk = 33
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -50,34 +50,22 @@ dependencies {
     // Kotlin standard library
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.7.20")
 
-    // Lifecycle components
-    implementation("android.arch.lifecycle:extensions:1.1.1")
-    implementation("androidx.lifecycle:lifecycle-common-java8:2.4.1")
-
-    // AndroidX
-    implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("androidx.core:core-ktx:1.8.0-alpha03")
-    implementation("androidx.activity:activity-ktx:1.4.0")
-    implementation("androidx.fragment:fragment-ktx:1.4.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
-
-    // Dagger 2
-    implementation("com.google.dagger:dagger:2.28.3")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.1.1")
-    implementation("androidx.compose.material3:material3:1.0.0-alpha02")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.1.1")
-    kapt("com.google.dagger:dagger-compiler:2.27")
-    compileOnly("org.glassfish:javax.annotation:3.1.1")
-
     // Hilt
     implementation("com.google.dagger:hilt-android:2.42")
     kapt("com.google.dagger:hilt-android-compiler:2.42")
 
     // Google Material IO
     implementation("com.google.android.material:material:1.5.0")
-    implementation("androidx.compose.material3:material3:1.0.1")
-    implementation("androidx.compose.material3:material3-window-size-class:1.0.1")
+    implementation("com.google.android.material:compose-theme-adapter:1.1.3")
+
+    // AndroidX
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("androidx.core:core-ktx:1.8.0-alpha03")
+    implementation("androidx.fragment:fragment-ktx:1.4.1")
+    implementation("androidx.activity:activity-compose:1.4.0")
+    implementation("androidx.activity:activity-ktx:1.4.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0-alpha02")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.4.1")
 
     // Jetpack Compose
     implementation("androidx.compose.ui:ui:1.1.0")
@@ -86,13 +74,10 @@ dependencies {
     implementation("androidx.compose.material:material:1.1.0")
     implementation("androidx.compose.material:material-icons-core:1.1.0")
     implementation("androidx.compose.material:material-icons-extended:1.1.0")
-    implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0-alpha02")
+    implementation("androidx.compose.material3:material3:1.0.0-alpha02")
     implementation("androidx.compose.runtime:runtime-livedata:1.1.0")
     implementation("androidx.compose.runtime:runtime-rxjava2:1.1.0")
-
-    // Jetpack Compose - MDC Theme
-    implementation("com.google.android.material:compose-theme-adapter:1.1.3")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.0")
 
     // Unit Testing
     testImplementation("junit:junit:4.13.2")
@@ -107,18 +92,10 @@ dependencies {
     androidTestImplementation("androidx.test:rules:1.4.0")
     testImplementation("org.hamcrest:hamcrest-all:1.3")
 
-    // UI Testing - Jetpack Compose
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.0")
-
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.8.1")
     implementation("com.squareup.retrofit2:adapter-rxjava2:2.4.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.8.1")
-    implementation("com.squareup.retrofit2:retrofit-mock:2.2.0")
     implementation("com.squareup.retrofit2:converter-gson:2.3.0")
-
-    // GSON
-    implementation("com.google.code.gson:gson:2.8.6")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
--- SourceCode/build.gradle.kts
+++ SourceCode/build.gradle.kts
@@ -1,12 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 buildscript {
     repositories {
         google()
@@ -18,5 +10,16 @@ buildscript {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
+    }
+}
+
+plugins {
+    id("com.autonomousapps.dependency-analysis") version "1.18.0"
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
     }
 }
