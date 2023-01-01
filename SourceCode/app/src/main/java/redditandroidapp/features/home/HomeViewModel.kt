b4efb9c6941c2f123a30470eb5d6c6ceebb7d95b
Rafal Ozog <rafalozog@gmail.com>
2023-01-01 11:19:04 +0000

    Architecture refactoring - architecture from Jetcaster project further implementation.

--- SourceCode/app/src/main/java/redditandroidapp/features/home/HomeViewModel.kt
+++ SourceCode/app/src/main/java/redditandroidapp/features/home/HomeViewModel.kt
@@ -1,35 +1,22 @@
-/*
- * Copyright 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package redditandroidapp.features.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
+import redditandroidapp.data.repositories.PostsRepository
+import javax.inject.Inject
 
-class HomeViewModel(
-//    private val podcastsRepository: PodcastsRepository = Graph.podcastRepository,
-//    private val podcastStore: PodcastStore = Graph.podcastStore
-) : ViewModel() {
+@HiltViewModel
+class HomeViewModel @Inject constructor(private val postsRepository: PostsRepository): ViewModel() {
     // Holds our currently selected home category
-    private val selectedCategory = MutableStateFlow(HomeCategory.Discover)
+//    private val selectedCategory = MutableStateFlow(HomeCategory.Discover)
     // Holds the currently available home categories
-    private val categories = MutableStateFlow(HomeCategory.values().asList())
+//    private val fetchedPosts = MutableStateFlow(emptyList())
 
     // Holds our view state which the UI collects via [state]
     private val _state = MutableStateFlow(HomeViewState())
@@ -41,27 +28,24 @@ class HomeViewModel(
 
     init {
         viewModelScope.launch {
-            // Combines the latest value from each of the flows, allowing us to generate a
-            // view state instance which only contains the latest values.
-//            combine(
-//                categories,
-//                selectedCategory,
+//             Combines the latest value from each of the flows, allowing us to generate a
+//             view state instance which only contains the latest values.
+            combine(
 //                podcastStore.followedPodcastsSortedByLastEpisode(limit = 20),
-//                refreshing
-//            ) { categories, selectedCategory, podcasts, refreshing ->
-//                HomeViewState(
-//                    homeCategories = categories,
-//                    selectedHomeCategory = selectedCategory,
-//                    featuredPodcasts = podcasts,
-//                    refreshing = refreshing,
-//                    errorMessage = null /* TODO */
-//                )
-//            }.catch { throwable ->
-//                // TODO: emit a UI error here. For now we'll just rethrow
-//                throw throwable
-//            }.collect {
-//                _state.value = it
-//            }
+                refreshing,
+                refreshing
+            ) { refreshing, refreshing2 ->
+                HomeViewState(
+//                    redditPosts = redditPosts,
+                    refreshing = refreshing,
+                    errorMessage = null /* TODO */
+                )
+            }.catch { throwable ->
+                // TODO: emit a UI error here. For now we'll just rethrow
+                throw throwable
+            }.collect {
+                _state.value = it
+            }
         }
 
         refresh(force = false)
@@ -78,26 +62,12 @@ class HomeViewModel(
             refreshing.value = false
         }
     }
-
-    fun onHomeCategorySelected(category: HomeCategory) {
-        selectedCategory.value = category
-    }
-
-    fun onPodcastUnfollowed(podcastUri: String) {
-        viewModelScope.launch {
-//            podcastStore.unfollowPodcast(podcastUri)
-        }
-    }
-}
-
-enum class HomeCategory {
-    Library, Discover
 }
 
 data class HomeViewState(
 //    val featuredPodcasts: List<PodcastWithExtraInfo> = emptyList(),
     val refreshing: Boolean = false,
-    val selectedHomeCategory: HomeCategory = HomeCategory.Discover,
-    val homeCategories: List<HomeCategory> = emptyList(),
+//    val selectedHomeCategory: HomeCategory = HomeCategory.Discover,
+//    val homeCategories: List<HomeCategory> = emptyList(),
     val errorMessage: String? = null
 )
