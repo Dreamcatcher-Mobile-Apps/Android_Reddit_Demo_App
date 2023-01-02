74ac596a39da129b51cd923ad724600e3b41ddb2
Sam <sams@apadmi.com>
2023-01-02 18:50:42 +0000

    list updates

--- SourceCode/app/src/main/java/redditandroidapp/ui/home/Home.kt
+++ SourceCode/app/src/main/java/redditandroidapp/ui/home/Home.kt
@@ -1,16 +1,16 @@
 package redditandroidapp.ui.home
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
@@ -25,7 +25,8 @@ fun Home(
 ) {
     val viewState by viewModel.state.collectAsStateWithLifecycle()
     Surface(Modifier.fillMaxSize()) {
-        HomeContent(posts = viewState.redditPosts ?: emptyList())
+        HomeContent(posts = viewState.redditPosts ?: emptyList(),
+            )
     }
 }
 
@@ -36,13 +37,33 @@ fun HomeContent(
     PostsList(posts = posts)
 }
 
+fun LazyListState.isScrolledToEnd() =
+    layoutInfo.visibleItemsInfo.lastOrNull()?.index == layoutInfo.totalItemsCount - 1
+
+
 @Composable
 private fun PostsList(posts: List<RedditPostModel>) {
+    val listState = rememberLazyListState()
+
     // Use LazyRow when making horizontal lists
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
+    LazyColumn(
+        state = listState,
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
         items(posts.size) { index ->
             PostsListItem(posts[index])
         }
+    }
+
+    val endOfListReached by remember {
+        derivedStateOf {
+            listState.isScrolledToEnd()
+        }
+    }
+
+    LaunchedEffect(endOfListReached) {
+
     }
 }
 
@@ -53,18 +74,21 @@ private fun PostsListItem(post: RedditPostModel) {
             Image(
                 painter = rememberAsyncImagePainter(it.thumbnail),
                 contentDescription = null,
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .size(200.dp)
+                    .height(200.dp)
             )
+            Spacer(Modifier.height(16.dp))
             post.title?.let {
-                androidx.compose.material.Text(
+                Text(
                     text = it,
                     style = MaterialTheme.typography.subtitle1
                 )
             }
+            Spacer(Modifier.height(16.dp))
             post.author?.let {
-                androidx.compose.material.Text(
+                Text(
                     text = it,
                     style = MaterialTheme.typography.subtitle2
                 )
